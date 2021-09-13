82f3486a620981f436dbe1f111a225ddc05fbcbd
Kenny Ortega <kennyaoc@gmail.com>
2021-09-13 14:22:41 -0400

    Display books in frontend

--- js/file.js
+++ js/file.js
@@ -1,4 +1,5 @@
 let books = [{title : '' , author: '', id: 0}];
+const file = document.querySelector("#book")
 
 
 function addBooks(title,author) {
@@ -11,8 +12,28 @@ function removeBooks(index) {
   });
 }
 
+
 addBooks('test', 'testAuthor');
 console.log(books);
 addBooks('test2', 'testAuthor2');
-removeBooks(1);
+//removeBooks(1);
 console.log(books);
+
+function showBooks(){
+
+  books.forEach(i => {
+    var title = document.createElement("p")
+    title.innerText = `${i.title}`
+    file.appendChild(title)
+    var author = document.createElement("p")
+    author.innerText = `${i.author}`
+    file.appendChild(author)
+    var button = document.createElement("button");
+    button.textContent = "Remove"
+    button.setAttribute("id",i.id)
+    file.appendChild(button)
+  });
+  
+}
+
+showBooks();
