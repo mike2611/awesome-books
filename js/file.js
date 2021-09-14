e3e906357dcfad4bfb90d27880e45902b508cad5
Kenny Ortega <kennyaoc@gmail.com>
2021-09-14 14:10:54 -0400

    Refactor project using classes

--- js/file.js
+++ js/file.js
@@ -1,26 +1,53 @@
 class Book {
-
-
   constructor(arr) {
-    this.books = arr
+    this.books = arr;
+    this.file = document.querySelector('.book');
   }
 
   get book() {
-    return this.books
+    return this.books;
   }
-  
-  removeBook(index) {
-  	this.books = this.books.filter((book) => book.id !== index);
-  }
-  
-  addBook(ti, aut, id) {
-  	console.log(typeof this.books)
-  	this.books.push({ title: ti, author: aut, id: this.books.length });
-  }
-  
-  
-}
 
+  removeBook(index) {
+    this.books = this.books.filter((book) => book.id !== index);
+    const element = document.getElementById(index);
+    element.parentNode.removeChild(element);
+    this.updateLocalStorage();
+  }
+
+  addBook(ti, aut) {
+    this.books.push({ title: ti, author: aut, id: this.books.length });
+    this.updateLocalStorage();
+  }
+
+  showBooks() {
+    for (let i = 0; i < this.books.length; i += 1) {
+      const article = document.createElement('article');
+      const title = document.createElement('p');
+      const author = document.createElement('p');
+      const button = document.createElement('button');
+
+      title.innerText = `${this.books[i].title}`;
+      author.innerText = `${this.books[i].author}`;
+      button.textContent = 'Remove';
+
+      article.setAttribute('id', i);
+      button.setAttribute('class', 'rmv-btn');
+      button.setAttribute('id', `rmv-${i}`);
+      button.addEventListener('click', () => this.removeBook(i));
+
+      article.appendChild(title);
+      article.appendChild(author);
+      article.appendChild(button);
+
+      this.file.appendChild(article);
+    }
+  }
+
+  updateLocalStorage() {
+    window.localStorage.setItem('cataloge', JSON.stringify(this.books));
+  }
+}
 
 let arr;
 
@@ -30,55 +57,13 @@ if (JSON.parse(localStorage.getItem('cataloge'))) {
   arr = [];
 }
 
-
-let books = new Book(arr);
-
-const file = document.querySelector('.book');
-
-function addBooks(ti, aut) {
-  books.push({ title: ti, author: aut, id: books.length });
-  window.localStorage.setItem('cataloge', JSON.stringify(books));
-}
-
-function removeBooks(index) {
-  books = books.filter((book) => book.id !== index);
-
-  const element = document.getElementById(index);
-  element.parentNode.removeChild(element);
-
-  window.localStorage.setItem('cataloge', JSON.stringify(books));
-}
-
-function showBooks() {
-  for (let i = 0; i < books.length; i += 1) {
-    const article = document.createElement('article');
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const button = document.createElement('button');
-
-    title.innerText = `${books[i].title}`;
-    author.innerText = `${books[i].author}`;
-    button.textContent = 'Remove';
-
-    article.setAttribute('id', i);
-    button.setAttribute('class', 'rmv-btn');
-    button.setAttribute('id', `rmv-${i}`);
-    button.addEventListener('click', () => removeBooks(i));
-
-    article.appendChild(title);
-    article.appendChild(author);
-    article.appendChild(button);
-
-    file.appendChild(article);
-  }
-}
-
-showBooks();
+const books = new Book(arr);
+books.showBooks();
 
 const formAdd = document.querySelector('#add-btn');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 
 formAdd.addEventListener('click', () => {
-  addBooks(titleInput.value, authorInput.value);
+  books.addBook(titleInput.value, authorInput.value);
 });
